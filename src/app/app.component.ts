6c8ceefec6f0c914af8c86b4005f320b2316bea7
Azad Singh <azad.singh@appster.in>
2018-12-25 16:16:58 +0530

    assignment done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tansent';
+  numberValue: number[] = [];
+  numberValue2: any;
+  numberValueArrLength: number;
+
+  onNumberChange(number: number = 100) {
+    let inputArray = [];
+    for (let i = 1; i <= number; i++) {
+      inputArray.push(i);
+    }
+    this.numberValue = inputArray.reduce((resultArray, item, index) => {
+      const chunkIndex = Math.floor(index / 10)
+      if (!resultArray[chunkIndex]) {
+        resultArray[chunkIndex] = [] // start a new chunk
+      }
+      resultArray[chunkIndex].push(item)
+      return resultArray
+    }, []);
+    let arr = [];
+    for (let i = 0; i < this.numberValue.length; i++) {
+      arr.push(Object.assign({}, this.numberValue[i]));
+    }
+    this.numberValue2 = arr;
+  }
+  getSum(data) {
+    return data.reduce((a, b) => a + b, 0);
+  }
+  getColumnSum(index) {
+    if (index < 10) {
+      let sum = 0;
+      for (let j = 0; j < this.numberValue2.length; j++) {
+        sum += this.numberValue2[j][index];
+      }
+      return sum;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { NumberToArrayPipe } from './number-to-array.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NumberToArrayPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
